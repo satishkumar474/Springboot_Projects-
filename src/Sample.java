3d58d1615218c8643b4698954aeb9a19507b9c0f
ABBS <ABBS@DESKTOP-GGA4P0P>
2024-03-05 22:25:01 +0530

    updated

--- src/Sample.java
+++ src/Sample.java
@@ -4,6 +4,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Commit");
+		System.out.println("Second Commit");
 
 	}
 
